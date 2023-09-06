efba75dc1e3891a6b3ff55974f8437e6f451ad7e
martijnbentum <bentummartijn@gmail.com>
2023-09-06 09:33:58 +0200

    use dict that map word to filenames of audio and textgrid to create word objects; added select_word function to select words based on criteria such as number of syllables

--- word.py
+++ word.py
@@ -1,4 +1,5 @@
 import metadata
+from progressbar import progressbar
 import time
 
 class Words:
@@ -9,21 +10,24 @@ class Words:
         self.mald_data = metadata.mald_word_data()
         self.baldey_header = metadata.baldey_header()
         self.mald_header = metadata.mald_word_header()
+        self.word_to_filenames = metadata.word_to_filenames_dict()
+        self._make_words()
 
-    def make_words(self):
+    def _make_words(self):
         self.words = []
         for line in self.baldey_data:
             word = line[9]
-            d = metadata.word_to_filenames(word, dataset_name = 'baldey')
+            d = self.word_to_filenames['baldey'][word]
             d['header'] = self.baldey_header
             d['metadata'] = line
             self.words.append(Word(**d))
         for line in self.mald_data:
             word = line[0]
-            d = metadata.word_to_filenames(word,dataset_name = 'mald')
+            d = self.word_to_filenames['mald'][word]
             d['header'] = self.mald_header
             d['metadata'] = line
             self.words.append(Word(**d))
+
 
 class Word:
     def __init__(self, word, audio_filename, textgrid_filename, 
@@ -35,29 +39,47 @@ class Word:
         self.header = header
         if len(self.header) == 50: self.dataset = 'baldey'
         else: self.dataset = 'mald'
+        self._set_info()
 
     def __repr__(self):
-        m = self.word
+        m = self.dataset + '| ' + self.word + ' ' +str(self.is_word)
+        m += ' ' + str(self.n_syllables)
         return m
     
     def _set_info(self):
         self.d = {}
         for md, column_name in zip(self.metadata, self.header):
             self.d[column_name] = md
-        self.is_word = self.d['word_status']
         if self.dataset == 'baldey': self._set_baldey()
         else: self._set_mald()
 
     def _set_mald(self):
+        self.is_word = self.d['word_status']
         self.n_syllables = self.d['n_syllables']
         self.duration = self.d['duration']
         self.pos = self.d['pos']
         self.phonemes = self.d['phoneme_transcription']
+        self.language = 'english'
 
     def _set_baldey(self):
+        if self.d['word_status'] == 'word': self.is_word = True
+        else: self.is_word = False
         self.n_syllables = self.d['Nword_syllables']
         self.duration = self.d['word_duration']
         self.pos = self.d['word_class']
         self.phonemes = self.d['transcription']
+        self.language = 'dutch'
             
+
+def select_words(words, n_syllables = None, language = None, is_word = None):
+    output = words
+    if n_syllables:
+        output = [word for word in output if word.n_syllables == n_syllables]
+    if language:
+        output = [word for word in output if word.language == language]
+    if is_word:
+        output = [word for word in output if word.is_word == is_word]
+    return output
+        
+    pass
 
