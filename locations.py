feeedf057642029fa106651d6309ec79b3bffd8c
martijnbentum <bentummartijn@gmail.com>
2023-09-07 17:24:28 +0200

    added celex and mald locations

--- locations.py
+++ locations.py
@@ -18,5 +18,11 @@ mald_word_data_filename = mald + 'MALD1_ItemData.txt'
 mald_response_data_filename = mald + 'ResponseData.txt'
 mald_subject_data_filename = mald + 'MALD1_SubjectData.txt'
 mald_wordset_filename = mald + 'word_set.txt'
+mald_table_directory = mald + 'tables/'
+mald_prosodic_syllables_directory = mald + 'prosodic_syllables/'
 
 word_to_filenames_dict = '../word_to_filenames_dict.json'
+
+celex_english_phonology_directory = '../CELEX/EPL/'
+celex_english_phonology_file = celex_english_phonology_directory + 'EPL.CD'
+celex_english_phonology_header = celex_english_phonology_directory + 'EPL.CD'
