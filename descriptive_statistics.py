522d76774c7d7aaa5b75bd2de16bd5b2f5fad01a
martijnbentum <bentummartijn@gmail.com>
2023-09-25 17:05:01 +0200

    added functions to analyze mald data

--- descriptive_statistics.py
+++ descriptive_statistics.py
@@ -25,16 +25,18 @@ def collect_syllables_celex(c = None, language = 'dutch'):
 def count_syllables(syllables):
     return Counter(syllables)
 
-def baldey_syllable_dict(syllables = None, w = None):
-    if not syllables: syllables = collect_syllables_ld(w, language = 'dutch')
+def _syllable_dict(syllables):
     d = {}
     for syllable in syllables:
         if syllable.ipa not in d.keys(): d[syllable.ipa] = []
         d[syllable.ipa].append( syllable )
     return sort_dict_on_number_of_entries(d)
 
-def stress_variability(syllable_dict=None, minimal_count = 10, perc_delta = .2):
-    if not syllable_dict: syllable_dict = baldey_syllable_dict()
+def baldey_syllable_dict(syllables = None, w = None):
+    if not syllables: syllables = collect_syllables_ld(w, language = 'dutch')
+    return _syllable_dict(syllables)
+
+def _stress_variability(syllable_dict, minimal_count, perc_delta):
     d = {}
     variable = {}
     for ipa, syllables in syllable_dict.items():
@@ -53,21 +55,27 @@ def stress_variability(syllable_dict=None, minimal_count = 10, perc_delta = .2):
                 'syllables':syllables,'perc_stressed':perc_stressed}
     return d, variable
 
+def stress_variability_baldey(syllable_dict = None, minimal_count = 10, 
+        perc_delta = .2):
+    if not syllable_dict: syllable_dict = baldey_syllable_dict()
+    return _stress_variability(syllable_dict, minimal_count, perc_delta)
+
 def sort_dict_on_number_of_entries(d):
     d =dict(sorted(d.items(), key = lambda item:len(item[1]),reverse = True))
     return d
 
-        
-def hapax_baldey_syllables(d):
-    if not d: d = baldey_syllable_dict()
+def _hapax_syllables(d):
     keys = [k for k,syllables in d.items() if len(syllables) ==1]
     hapax= []
     for k in keys: 
         hapax.extend( d[k] )
     return hapax
-
-def top_n_syllable_type_count(d, n = 20):
+        
+def hapax_baldey_syllables(d= None):
     if not d: d = baldey_syllable_dict()
+    return _hapax_syllables(d)
+
+def _top_n_syllable_type_count(d, n = 20):
     keys = list(d.keys())
     for k in keys[:n]:
         stressed = str(len([x for x in d[k] if x.stressed]))
@@ -78,4 +86,27 @@ def top_n_syllable_type_count(d, n = 20):
         m += word+'|'+ipa+'|'
         print(m)
    
+def top_n_syllable_count_baldey(d = None, n = 20):
+    if not d: d = baldey_syllable_dict()
+    _top_n_syllable_type_count(d,n)
+
+# ------------------------------------------------------------------------------
+# mald
+    
+def mald_syllable_dict(syllables = None, w = None):
+    if not syllables: syllables = collect_syllables_ld(w, language = 'english')
+    return _syllable_dict(syllables)
+
+def hapax_mald_syllables(d = None):
+    if not d: d = mald_syllable_dict()
+    return _hapax_syllables(d)
+
+def top_n_syllable_count_mald(d = None, n = 20):
+    if not d: d = mald_syllable_dict()
+    _top_n_syllable_type_count(d,n)
+
+def stress_variability_mald(syllable_dict = None, minimal_count = 10,
+        perc_delta = .2):
+    if not syllable_dict: syllable_dict = mald_syllable_dict()
+    return _stress_variability(syllable_dict, minimal_count, perc_delta)
     
