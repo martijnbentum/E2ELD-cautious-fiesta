15ec36d66375cbdb72ae145cebe4b9db3fb94e53
martijnbentum <bentummartijn@gmail.com>
2023-12-15 17:39:45 +0100

    module to compute intensity of frequency band / work in progress

---
+++ frequency_band.py
@@ -0,0 +1,30 @@
+import librosa
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_audio_file(file_path, sample_rate = 16000):
+    signal, sr = librosa.load(file_path, sr=sample_rate)
+    return signal, sr
+
+def compute_fft(signal, sample_rate = 16000):
+    fft_result= np.fft.fft(signal)
+    frequencies = np.fft.fftfreq(len(signal), 1.0/sample_rate)
+    frequencies = frequencies[:int(len(frequencies)/2)] 
+    fft_result = fft_result[:int(len(fft_result)/2)]
+    return frequencies, fft_result
+
+def compute_power_spectrum(signal, sample_rate = 16000):
+    frequencies, fft_result = compute_fft(signal, sample_rate)
+    power_spectrum = np.abs(fft_result)**2 / len(signal)
+    return frequencies, power_spectrum
+
+def plot_power_spectrum(signal, sample_rate = 16000):
+    frequencies, power_spectrum = compute_power_spectrum(signal, sample_rate)
+    plt.ion()
+    plt.clf()
+    plt.plot(frequencies, power_spectrum)
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Power')
+    plt.grid(alpha=0.3)
+    plt.show()
+
