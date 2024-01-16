5d06077c7db913a19fb7508adef5bbcc9ffaff43
martijnbentum <bentummartijn@gmail.com>
2024-01-16 12:24:03 +0100

    added code to create X y codevector dataset; check frames beforehand to check whether they are ok to determine X and y size; added code to save and load the arrays

--- codevectors_lda.py
+++ codevectors_lda.py
@@ -9,15 +9,40 @@ def get_frames(w = None):
     if not w: w = word.get_frames()
     return codevectors.Frames(w)
 
-def make_dataset(frames = None, w = None):
+def count_ok_frames(frames):
+    indices = []
+    for index, frame in enumerate(frames):
+        if not hasattr(frame, 'phoneme') 
+        if not frame.phoneme: continue
+        if not hasattr(frame.codevector): continue
+        if not hasattr(frame.codevector.shape): continue
+        if not frame.codevector.shape[0] == codevector_size: continue
+        indices.append(index)
+    return indices
+
+def make_dataset(frames = None, w = None, save = False):
     '''convert codevector frames to numpy arrays
     for LDA training.
     '''
     if not frames: frames = get_frames(w = w).frames
     frame = frames[0]
-    X = np.zeros((len(frames), len(frame.codevector)))
-    y = np.zeros((len(frames), 1))
-    for index, frame in enumerate(frames):
+    codevector_size = frame.codevector.shape[0]
+    indices = count_ok_frames(frames)
+    X = np.zeros((len(indices), codevector_size))
+    y = np.zeros((len(indices), 1))
+    for index in indices:
+        frame = frames[index]
         X[index] = frame.codevector
         y[index] = int(frame.phoneme.stressed)
+    if save:
+        np.savez('../MALD/codevectors.npz', X = X, y = y)
     return X, y
+
+def load_dataset():
+    '''load codevector dataset from disk'''
+    if not os.path.exists('../MALD/codevectors.npz'):
+        return make_dataset(save = True)
+    data = np.load('../MALD/codevectors.npz')
+    return data['X'], data['y']
+
+
