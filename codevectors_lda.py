3e5bd803658f3a7da072302797aa3927e930a6fd
martijnbentum <bentummartijn@gmail.com>
2024-01-16 13:37:09 +0100

    checking frames takes long time; storing indices of frames that are ok

--- codevectors_lda.py
+++ codevectors_lda.py
@@ -3,6 +3,7 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 import numpy as np
+import os
 from progressbar import progressbar
 import word
 
@@ -10,27 +11,34 @@ def get_frames(w = None):
     if not w: w = word.get_frames()
     return codevectors.Frames(w)
 
-def count_ok_frames(frames):
+def check_frames_ok(frames = None, save = False):
+    if not frames: frames = get_frames(w = w).frames
     indices = []
-    codevector_size = frames[0].codevector.shape[0]
-    for index, frame in enumerate(frames):
+    print('checking frames for ok, returing ok indices')
+    for index, frame in enumerate(progressbar(frames)):
         if not hasattr(frame, 'phoneme'): continue 
         if not frame.phoneme: continue
-        if not hasattr(frame,'codevector'): continue
-        if not hasattr(frame.codevector,'shape'): continue
-        if not frame.codevector.shape[0] == codevector_size: continue
         indices.append(index)
+    if save:
+        np.save('../MALD/frame_ok_indices.npy', indices)
     return indices
 
-def make_dataset(frames = None, w = None, save = False):
+def load_frames_ok_indices(frames = None):
+    if not os.path.exists('../MALD/frame_ok_indices.npy'):
+        return check_frames_ok(frames = frames, save = True)
+    return np.load('../MALD/frame_ok_indices.npy')
+
+def make_dataset(frames = None, w = None, 
+    save = False):
     '''convert codevector frames to numpy arrays
     for LDA training.
     '''
     if not frames: frames = get_frames(w = w).frames
+    indices = load_frames_ok_indices(frames)
     codevector_size = frames[0].codevector.shape[0]
-    indices = count_ok_frames(frames)
     X = np.zeros((len(indices), codevector_size))
-    y = np.zeros((len(indices), 1))
+    y = np.zeros((len(indices), 1), dtype = np.int8)
+    print('making codevector dataset')
     for index in progressbar(indices):
         frame = frames[index]
         X[index] = frame.codevector
