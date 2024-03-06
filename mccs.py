c2f77f7e9080a017541af9aa7f9d2128c522e866
martijnbentum <bentummartijn@gmail.com>
2024-03-06 10:21:17 +0100

    moved compute_mean_sem_ci to general; layout update

--- mccs.py
+++ mccs.py
@@ -1,9 +1,10 @@
 import json
 from matplotlib import pyplot as plt
 import numpy as np
+import general
 
 spectral_balance_filename = '../mccs_density_lda_spectral_balance.json'
-accoustic_correlates_filename = '../mccs_density_clf_acoustic_correlates.json'
+accoustic_correlates_filename='../mccs_density_clf_acoustic_correlates.json'
 f1f1_filename = '../mccs_lda_f1f2.json'
 combined_filename = '../mccs_combined_ac_lda_clf.json'
 filenames = [spectral_balance_filename, accoustic_correlates_filename, 
@@ -50,8 +51,8 @@ def plot_mccs(new_figure=True, add_perceptron = False):
     x = range(len(results))
     means = [stats[key]['mean'] for key in stats]
     cis = [stats[key]['ci'] for key in stats]
-    plt.errorbar(x, means, yerr=cis, fmt=',', markersize = 12, color = 'black',
-        elinewidth = 2.5, capsize = 9, capthick = .6)
+    plt.errorbar(x, means, yerr=cis, fmt=',', markersize = 12, 
+        color = 'black',elinewidth = 2.5, capsize = 9, capthick = .6)
     plt.grid(alpha=0.3)
     plt.xticks(range(len(results)), results.keys(), rotation=45)
     plt.ylabel("matthew's correlation coefficient")
@@ -59,15 +60,13 @@ def plot_mccs(new_figure=True, add_perceptron = False):
     # plt.yticks(y, [str(round(i,2)) for i in y])
 
 
-def compute_mean_sem_ci(data, ci_value=2.576):
-    mean = np.mean(data)
-    sem = np.std(data) / np.sqrt(len(data))
-    ci = ci_value * sem
-    return mean, sem, ci
 
 def compute_mccs_stats(results):
     output ={}
-    for key, value in results.items():
-        mean, sem, ci= compute_mean_sem_ci(value)
+    for key, data in results.items():
+        mean, sem, ci= general.compute_mean_sem_ci(data)
         output[key] = {'mean': mean, 'sem': sem, 'ci': ci}
     return output
+
+
+
